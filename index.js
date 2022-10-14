494e423f6de691835670a829731e6f8a36ed3c7e
Steve <stevesantner@gmail.com>
2022-10-13 21:28:59 -0400

    commit tree 7a45422c93b4b848b60a67949586cf448f30c33d

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+    // we'll fill this in shortly
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    // we'll fill this in shortly
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    // we'll fill this in shortly
+    return a * b;
+  }
+
+  function divide(a, b) {
+    // we'll fill this in shortly
+    return a / b;
+  }
+
+  function increment(a) {
+    // it fuckin worked
+    a++;
+    return a;
+  }
+
+  function decrement(a) {
+    a--;
+    return a;
+  }
+
+  function preserveDecimal(n) {
+    parseFloat(n);
+    return n;
+  }
+
+
+  function makeInt(n) {
+    parseInt(n);
+    return n;
+
+  }
+
+  
